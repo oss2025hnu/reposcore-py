d0e367ea75f1f71d818663a71a78d328d0b4f973
이제우 <ljw78633@gmail.com>
2025-04-13 20:55:34 +0900

    Update __main__.py

--- reposcore/__main__.py
+++ reposcore/__main__.py
@@ -12,6 +12,7 @@ import logging
 
 # logging 모듈 기본 설정 (analyzer.py와 동일한 설정)
 logging.basicConfig(
+    stream=sys.stdout,
     level=logging.INFO,
     format='[%(asctime)s] %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S'
